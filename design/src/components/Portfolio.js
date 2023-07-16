526eff93bddc7a6e43116a7785bf731faf7ddbf4
livia-varga <varga.livia86@gmail.com>
2023-07-16 10:20:20 +0200

    css

--- design/src/components/Portfolio.js
+++ design/src/components/Portfolio.js
@@ -2,24 +2,28 @@ import '../styles/portfolio.css';
 import Blissfulbitesfigma from '../styles/pics/Blissfulbitesfigma.png';
 import Blissfulmobilesmall from '../styles/pics/Blissfulmobilesmall.png';
 import Desktopsmall from '../styles/pics/Desktopsmall.png';
+import cutcolor11 from '../styles/pics/cutcolor11.png';
+import cutcolor12 from '../styles/pics/cutcolor12.png';
 
 
 const Home = () => {
     return (
         <>
-        <body>    
-            <div className="home">
-               <h1 className="welcomehome">Portfolio</h1> 
-               <div className="portfolio-container">
-                <div className="wrapper">
-                <img src ={Blissfulbitesfigma} className="imgportfolio" alt='' />
-               <img src ={Blissfulmobilesmall} className="imgportfolio" alt='' />
-               <img src ={Desktopsmall} className="imgportfolio2" alt='' />
-               </div>
-               </div>
-            </div>
-            
-        </body>
+            <body>
+                <div className="home">
+                    <h1 className="welcomehome">Portfolio</h1>
+                    <div className="portfolio-container">
+                        <div className="wrapper">
+                            <img src={cutcolor12} className="imgportfolio" alt='' />
+                            <img src={cutcolor11} className="imgportfolio" alt='' />
+                            <img src={Blissfulbitesfigma} className="imgportfolio" alt='' />
+                            <img src={Blissfulmobilesmall} className="imgportfolio" alt='' />
+                            <img src={Desktopsmall} className="imgportfolio2" alt='' />
+                        </div>
+                    </div>
+                </div>
+
+            </body>
         </>
     )
 }
